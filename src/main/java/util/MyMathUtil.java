e6e2a7095b6fd891d3f52555909d25220aae5a10
sonchanwoo <chanchan1116@naver.com>
2022-01-09 12:03:55 +0900

    refactor MyMathUtil Class

--- src/main/java/util/MyMathUtil.java
+++ src/main/java/util/MyMathUtil.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyMathUtil {
-    public static boolean checkDigit(int i, int digit) {
-        int n = (int)Math.pow(10,digit);
-        return i/n == 0 && i/(n/10) != 0;
+    //check는 긍정으로 통일하기로 했다
+    //성능과 가독성이 함께하지 않을 경우가 있다
+    private static final int DECIMAL_NUM=10;
+    private static final int ZERO=0;
+
+    public static boolean checkDigit(int num, int digit) {
+        int n = num/(int)Math.pow(DECIMAL_NUM,digit-1);
+        return n/DECIMAL_NUM == ZERO && n != ZERO;
     }
 
     public static List<Integer> divide(int num) {
         List<Integer> list = new ArrayList<>();
 
-        for(int i=1;num/i!=0;i*=10){
-            list.add(0,(num/i)%10);
+        int share = num*DECIMAL_NUM;
+        while((share/=DECIMAL_NUM)!=ZERO){
+            list.add(ZERO,(share)%DECIMAL_NUM);
         }
 
         return list;
