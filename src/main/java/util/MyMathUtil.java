8f3baa5c94c78f0a177fa8329d72990669d1e3c4
sonchanwoo <chanchan1116@naver.com>
2022-01-09 11:32:33 +0900

    modify MyMathUtil

--- src/main/java/util/MyMathUtil.java
+++ src/main/java/util/MyMathUtil.java
@@ -12,12 +12,10 @@ public class MyMathUtil {
     public static List<Integer> divide(int num) {
         List<Integer> list = new ArrayList<>();
 
-        //더 좋은 방법 없을까
-        //digit을 받아서 카운트처리?
-        //digit받지않고 할만한..방법
         for(int i=1;num/i!=0;i*=10){
             list.add(0,(num/i)%10);
         }
+
         return list;
     }
 }
--- src/test/java/util/MyMathUtilTests.java
+++ src/test/java/util/MyMathUtilTests.java
@@ -13,20 +13,14 @@ public class MyMathUtilTests {
     @ParameterizedTest
     @CsvSource(value = {"11:false","111:true","1111:false"},delimiter = ':')
     @DisplayName("파라미터로 전달된 값이, 파라미터로 전달된 자릿수 인지 판단")
-    void test5(int input,boolean output){
+    void testCheckDigit(int input,boolean output){
         assertThat(MyMathUtil.checkDigit(input,3)).isEqualTo(output);
     }
 
     @Test
     @DisplayName("사용자가 입력한 3숫자를 각각으로 나눈다")
     void testDivide(){
-        //이 테스트코드..리팩토링할수있을까
         List<Integer> dividedNums=MyMathUtil.divide(123);
-
-        //string[]이 아닌지 containsExactly를 사용할 수 없네..
-        //String.charAt + parse버전으로 바꾸어야하나..
-        assertThat(dividedNums.contains(1)).isTrue();
-        assertThat(dividedNums.contains(2)).isTrue();
-        assertThat(dividedNums.contains(3)).isTrue();
+        assertThat(dividedNums).containsExactly(1,2,3);
     }
 }
