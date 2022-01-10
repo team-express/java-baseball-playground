246cd62dc2357ecbb4584443ba19c5b97a26456c
sonchanwoo <chanchan1116@naver.com>
2022-01-10 16:26:43 +0900

    돌아는 가지만 리팩토링안했고 사용자 잘못입력 처리 안함 main부분 극혐이기도 하고

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -8,12 +8,13 @@ public class Main {
         BallMaker ballMaker = new BallMaker(1,9,3);
         BallMatcher ballMatcher = new BallMatcher();
 
+        Balls computerBalls = ballMaker.createComputerBalls();
+
         int userChoice=1;
 
         while(userChoice==1){
             System.out.print("숫자를 입력해 주세요 : ");
             Balls userBalls = ballMaker.createUserBalls(scanner.nextInt());
-            Balls computerBalls = ballMaker.createComputerBalls();
             BallStatesInfo ballStatesInfo = ballMatcher.getBallStatesInfo(userBalls,computerBalls);
             System.out.println(ballStatesInfo);
 
@@ -21,6 +22,7 @@ public class Main {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 userChoice = scanner.nextInt();
+                computerBalls = ballMaker.createComputerBalls();
             }
         }
     }
